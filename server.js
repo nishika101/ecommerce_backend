56ef6c4c05ab72c942e1237623ff5490d436b99d
nishika sahu <snishika852@gmail.com>
2025-07-30 01:57:27 +0530

    Day-1-form

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const cors = require('cors');
+const mysql2 = require('mysql2');
+
+
+dotenv.config();
